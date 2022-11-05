a42942212540346c46189d90cd96924d26997b1d
jaeyeong815 <jae0ee@naver.com>
2022-11-05 17:07:07 +0900

    feat: 사이드바, 메인 라우터 구성

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/common/Sidebar';
+import Main from './pages/Main';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </BrowserRouter>
   );
 };
